a4b1665cfb33ac759261822e1f10b2311d582b15
nbalochtech <nbalochtech@gmail.com>
2023-06-23 22:58:56 +0500

    work on context

--- src/App.js
+++ src/App.js
@@ -13,30 +13,39 @@ import Error from "./components/general/Error";
 import MovieDetails from "./components/movies/MovieDetails";
 import ApiDetails from "./components/apiwork/ApiDetails";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createContext } from "react";
+
+const SendData = createContext();
 
 function App() {
+  let allData = "Noman Baloch";
+  let userData = { name: "Noman Baloch", age: 30, isMarri: false };
+
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route path="/song" element={<Songs />} />
-            <Route path="/pic" element={<Pictures />} />
-          </Route>
-          <Route path="/about" element={<About />} />
-          <Route path="/text" element={<Text />} />
-          <Route path="/movies" element={<ShowMovies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} />
-          <Route path="/apidata" element={<APIdata />} />
-          <Route path="/apidata/:empId" element={<ApiDetails />} />
-          <Route path="/incdec" element={<Incdec className={App} />} />
-          <Route path="/users" element={<Userlist />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
+      <SendData.Provider value={{ allData, userData }}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="/song" element={<Songs />} />
+              <Route path="/pic" element={<Pictures />} />
+            </Route>
+            <Route path="/about" element={<About />} />
+            <Route path="/text" element={<Text />} />
+            <Route path="/movies" element={<ShowMovies />} />
+            <Route path="/movies/:movieId" element={<MovieDetails />} />
+            <Route path="/apidata" element={<APIdata />} />
+            <Route path="/apidata/:empId" element={<ApiDetails />} />
+            <Route path="/incdec" element={<Incdec className={App} />} />
+            <Route path="/users" element={<Userlist />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
+      </SendData.Provider>
     </>
   );
 }
 
 export default App;
+export { SendData };
--- src/components/apiwork/ApiDetails.jsx
+++ src/components/apiwork/ApiDetails.jsx
@@ -1,9 +1,12 @@
 // import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom';
 import * as Icon from "react-bootstrap-icons";
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { SendData } from '../../App';
 
 const ApiDetails = () => {
+    const contxt = useContext(SendData);
+    const navigation = useNavigate();
     const { empId } = useParams();
     const [empApi, setEmpApi] = useState({});
     useEffect(() => {
@@ -11,13 +14,12 @@ const ApiDetails = () => {
             const apiLink = await fetch(
                 `https://hub.dummyapis.com/employee?noofRecords=1&idStarts=${empId}`
             );
-
             const res = await apiLink.json();
             setEmpApi(res[0]);
         }
         getEmp();
-    }, []);
-    console.log(empApi);
+    }, [empId]);
+    console.log(contxt.userData.isMarri);
     return (
         <>
             <div className="container mt-5">
@@ -25,18 +27,22 @@ const ApiDetails = () => {
                     <div className="col-md-7">
                         <div className="card p-3 py-4">
                             <div className="text-center">
-                                <img src={empApi.imageUrl} width="100" className="rounded-circle" />
+                                <img src={empApi.imageUrl} alt={empApi.id} width="100" className="rounded-circle" />
                             </div>
                             <div className="text-center mt-3">
-                                <span className="bg-secondary p-1 px-4 rounded text-white">Pro</span>
+                                <span className="bg-secondary p-1 px-4 rounded text-white">{empApi.age}</span>
                                 <h5 className="mt-2 mb-0">{empApi.firstName} {empApi.lastName}</h5>
-                                <span></span>
+                                <span>{empApi.dob}</span>
                                 <div className="px-4 mt-1">
-                                    <p className="fonts">Consectetur adipiscing elit, sed do eiusmod </p>
+                                    <p className="fonts">{empApi.email}</p>
                                 </div>
                                 <div className="buttons">
-                                    <button className="btn btn-outline-primary px-4">Message</button>
-                                    <button className="btn btn-primary px-4 ms-3">Contact</button>
+                                    <button className="btn btn-outline-primary px-4">{empApi.contactNumber}</button>
+                                    <br /><br />
+                                    <button className="btn btn-warning px-5 ms-3" onClick={() => navigation(-1)}>
+                                        <Icon.Arrow90degLeft size={20} /> Go Back
+                                    </button>
+                                    <h4>{contxt.allData}</h4>
                                 </div>
                             </div>
                         </div>
