df3f68ffbc5eac704e4b578d09ff985533fd3fc4
nbalochtech <nbalochtech@gmail.com>
2023-06-24 22:56:24 +0500

    just update

--- src/components/general/Incdec.jsx
+++ src/components/general/Incdec.jsx
@@ -1,22 +1,56 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+
+
 
 const Incdec = () => {
-  const [counter, setCounter] = useState(0);
 
+  // const reducer = (state, action) => {
+  //   switch (action.type) {
+  //     case "Inc":
+  //       return state + 2;
+  //       break;
+  //     case "Dec":
+  //       return state - 2;
+  //       break;
+  //     case "Mul":
+  //       return state * 2;
+  //       break;
+  //     case "Dev":
+  //       return state / 2;
+  //       break;
+  //     default:
+  //       return state;
+  //       break;
+  //   }
+  // }
+  // const [state, dipatch] = useReducer(reducer, 0);
+  const [counter, setCounter] = useState(0);
   const dec = (n) => {
     setCounter(counter - n);
   };
   const inc = (n) => {
     setCounter(counter + n);
   };
+
   return (
-    <>
-      <h1>Increament and decreament</h1>
-      <h4>{counter}</h4>
-      <br />
-      <button onClick={() => dec(2)}> - Decreament</button>
-      <button onClick={() => inc(5)}> + Increament</button>
-    </>
+    <dev className="container center">
+
+      <div class="card text-center">
+        <div class="card-header">
+          <h1>Increament and decreament</h1>
+        </div>
+        <div class="card-body">
+          {/* <h1 class="card-title">{state}</h1>
+          <button className="btn btn-danger m-3" onClick={() => dipatch({ type: "Dec" })}> - Decreament</button>
+          <button className="btn btn-primary" onClick={() => dipatch({ type: "Inc" })}> + Increament</button>
+          <button className="btn btn-success m-3" onClick={() => dipatch({ type: "Mul" })}> * Multiply</button>
+          <button className="btn btn-warning" onClick={() => dipatch({ type: "Dev" })}> / devide</button>
+        </div> */}
+          <div class="card-footer text-muted">
+            Use Reduce
+          </div>
+        </div>
+    </dev>
   );
 };
 
