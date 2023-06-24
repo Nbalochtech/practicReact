1d1b18d06925e11ce2d7b796ee06f65668a03977
nbalochtech <nbalochtech@gmail.com>
2023-06-24 12:01:40 +0500

    work on useRef

--- src/components/apiwork/APIdata.jsx
+++ src/components/apiwork/APIdata.jsx
@@ -1,20 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const APIdata = () => {
-  // let numover = 10;
   const [numEmp, setNumEmp] = useState(10);
   let numover = numEmp < 1 ? 1 : numEmp;
   const [dataAPI, setDataAPI] = useState([]);
+  const [prevInputValue, setPrevInputValue] = useState([]);
+  const previnput = useRef();
+  const inputField = useRef();
+
+  const inputHanlde = (e) => {
+    setNumEmp(e.target.value);
+  };
+  const SearchHandle = () => {
+    console.log(inputField.current.value);
+  };
+
+  // useEffect(() => {
+  //   // counterRender.current = counterRender.current + 1;
+  //   previnput.current = numEmp;
+  //   setPrevInputValue([...prevInputValue, previnput.current]);
+  //   console.log(prevInputValue);
+  // }, [numEmp]);
 
   useEffect(() => {
     async function getData() {
       const apilinkdata = await fetch(
         `https://hub.dummyapis.com/employee?noofRecords=${numover}&idStarts=1001`
       );
-
       const res = await apilinkdata.json();
-
       setDataAPI(res);
     }
     getData();
@@ -25,11 +39,12 @@ const APIdata = () => {
         <h1>Employee List</h1>
         <input
           type="number"
+          ref={inputField}
           value={numEmp}
-          onChange={(e) =>
-            numEmp < 5 ? setNumEmp(5) : setNumEmp(e.target.value)
-          }
+          onChange={(e) => inputHanlde(e)}
         />
+        <button onClick={SearchHandle}>Search</button>
+        {/* <h3>{prevInputValue}</h3> */}
         <table className="table table-striped">
           <thead className="thead-dark">
             <tr>
@@ -53,7 +68,12 @@ const APIdata = () => {
                   <td>{data.dob}</td>
                   <td>{data.email}</td>
                   <td>
-                    <Link to={`/apidata/${data.id}`} className="btn btn-primary">Details</Link>
+                    <Link
+                      to={`/apidata/${data.id}`}
+                      className="btn btn-primary"
+                    >
+                      Details
+                    </Link>
                   </td>
                 </tr>
               );
