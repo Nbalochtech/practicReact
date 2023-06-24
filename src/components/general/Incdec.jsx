fcd142c495b6ae45fd838c21030a63df0fe7a584
nbalochtech <nbalochtech@gmail.com>
2023-06-24 23:05:07 +0500

    use reduceer  in Incdec.jsx file

--- src/components/general/Incdec.jsx
+++ src/components/general/Incdec.jsx
@@ -4,26 +4,26 @@ import React, { useReducer, useState } from "react";
 
 const Incdec = () => {
 
-  // const reducer = (state, action) => {
-  //   switch (action.type) {
-  //     case "Inc":
-  //       return state + 2;
-  //       break;
-  //     case "Dec":
-  //       return state - 2;
-  //       break;
-  //     case "Mul":
-  //       return state * 2;
-  //       break;
-  //     case "Dev":
-  //       return state / 2;
-  //       break;
-  //     default:
-  //       return state;
-  //       break;
-  //   }
-  // }
-  // const [state, dipatch] = useReducer(reducer, 0);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "Inc":
+        return state + 2;
+        break;
+      case "Dec":
+        return state - 2;
+        break;
+      case "Mul":
+        return state * 2;
+        break;
+      case "Dev":
+        return state / 2;
+        break;
+      default:
+        return state;
+        break;
+    }
+  }
+  const [state, dipatch] = useReducer(reducer, 0);
   const [counter, setCounter] = useState(0);
   const dec = (n) => {
     setCounter(counter - n);
@@ -40,16 +40,16 @@ const Incdec = () => {
           <h1>Increament and decreament</h1>
         </div>
         <div class="card-body">
-          {/* <h1 class="card-title">{state}</h1>
+          <h1 class="card-title">{state}</h1>
           <button className="btn btn-danger m-3" onClick={() => dipatch({ type: "Dec" })}> - Decreament</button>
           <button className="btn btn-primary" onClick={() => dipatch({ type: "Inc" })}> + Increament</button>
           <button className="btn btn-success m-3" onClick={() => dipatch({ type: "Mul" })}> * Multiply</button>
           <button className="btn btn-warning" onClick={() => dipatch({ type: "Dev" })}> / devide</button>
-        </div> */}
-          <div class="card-footer text-muted">
-            Use Reduce
-          </div>
         </div>
+        <div class="card-footer text-muted">
+          Use Reduce
+        </div>
+      </div>
     </dev>
   );
 };
